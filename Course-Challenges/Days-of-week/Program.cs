1819dc532d64474fa26057d0bb44aaea60b8d6b7
yasamin ashoori <yasaminashoori18@gmail.com>
2022-08-31 22:20:31 +0430

    new challenges and some changes added

---
+++ Course-Challenges/Days-of-week/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Days_of_week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string dayName;
+            Console.WriteLine("Please enter a number from 1 to  7 : ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            if (day == 1)
+            {
+                dayName = "Saturday";
+                Console.WriteLine("Today is Saturday");
+            }
+            else if (day == 2)
+            {
+                dayName = "Sunday";
+                Console.WriteLine("Today is Sunday");
+            }
+            else if (day == 3)
+            {
+                dayName = "Monday";
+                Console.WriteLine("Today is Munday");
+            }
+            else if (day == 4)
+            {
+                dayName = "Tuesday";
+                Console.WriteLine("Today is Tuesday");
+            }
+            else if (day == 5)
+            {
+                dayName = "Wednesday";
+                Console.WriteLine("Today is Wednesday");
+            }
+            else if (day == 6)
+            {
+                dayName = "Thursday";
+                Console.WriteLine("Today is Thursday");
+            }
+            else if (day == 7)
+            {
+                dayName = "Friday";
+                Console.WriteLine("Today is Friday");
+            }
+            else
+            {
+                Console.WriteLine("This is invalid name !");
+                
+            }
+
+        }
+    }
+}
--- my-practices/Variables/Program.cs
+++ my-practices/Variables/Program.cs
@@ -6,15 +6,18 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            const double PI = 3.14;
-            Console.WriteLine(PI);
-            
-            int x;
-            x = 150;
+            const double pi = 3.14;
+            Console.WriteLine(pi);
+            // const values couldn't chane and they are read-only values
+
+            int x; //declaration
+            x = 150; // initialization
+
+            bool status = false; // boolean data type
 
             double y = -12.498;
-            float small = 12.484f;
-            long bigInt = -1303039933;
+            float small = 12.484F;
+            long bigInt = 1303039933;
             Console.WriteLine("variables are " +  x +  y +  small +  bigInt);
             double first=12.3, second=194.43;
             Console.WriteLine($"the results are: {first} {second}");
